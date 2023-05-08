1ef163d1907a424df52bd4352dbc4adcecf7b5ef
ajlarson24 <123599236+ajlarson24@users.noreply.github.com>
2023-05-08 18:18:21 -0500

    got rid of legend

--- js/main.js
+++ js/main.js
@@ -353,26 +353,6 @@ info.update = function (props) {
 
 info.addTo(map);
 
-var legend = L.control({position: 'bottomleft'});
-
-legend.onAdd = function (map) {
-
-    var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-        labels = [];
-
-    // loop through our density intervals and generate a label with a colored square for each interval
-    for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
-            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
-    }
-
-    return div;
-};
-
-legend.addTo(map);*/
-
 
 /*
 On selection of point on map or input of address:
